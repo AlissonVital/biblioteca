9d56344dcaef1e70b65a536f101036f854644957
AlissonVital <alissonferreiravital@gmail.com>
2026-06-30 10:56:23 -0300

    Atualizado(a estrutura do projeto, alguns pacotes e classes), Criado o DB biblioteca e livros com fk)

---
+++ src/main/java/com/bibliotecaCentral/modules/bibliotecaCompany/entities/LivroEntity.java
@@ -0,0 +1,36 @@
+package com.bibliotecaCentral.modules.livro;
+
+import com.bibliotecaCentral.modules.biblioteca.BibliotecaEntity;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.stereotype.Component;
+import jakarta.persistence.Column;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Component
+@Data
+@Entity(name = "livro")
+public class LivroEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private String quantidadeTotal;
+    private String quantidadeDisponivel;
+
+    @ManyToOne()
+    @JoinColumn(name = "biblioteca_id", insertable = false, updatable = false)
+    private BibliotecaEntity bibliotecaEntity;
+
+    @Column(name = "biblioteca_id")
+    private UUID bibliotecaId;
+
+    @CreationTimestamp
+    private LocalDateTime dataCadastro;
+}
