1f08157a784818725dbb0416065fb327069d5f6a
AlissonVital <alissonferreiravital@gmail.com>
2026-07-01 09:02:26 -0300

    Criado as dependencias da documentação swagger

--- src/main/java/com/bibliotecaCentral/biblioteca/BibliotecaApplication.java
+++ src/main/java/com/bibliotecaCentral/biblioteca/BibliotecaApplication.java
@@ -1,5 +1,7 @@
 package com.bibliotecaCentral.biblioteca;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
@@ -11,10 +13,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "com.bibliotecaCentral")
 @ComponentScan(basePackages = "com.bibliotecaCentral")
 @EnableJpaRepositories(basePackages = "com.bibliotecaCentral")
+@OpenAPIDefinition(info = @Info(title = "Biblioteca Central", description = "API Responsável por gerenciar as locações de livros", version = "1"))
 public class BibliotecaApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(BibliotecaApplication.class, args);
-	}
+        SpringApplication.run(BibliotecaApplication.class, args);
+    }
 }
